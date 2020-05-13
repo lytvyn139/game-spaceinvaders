0d8c8f384583ed030b747df7bc1bdd12ea0fb410
IURII LYTVYN <lytvyn.139@gmail.com>
2020-05-13 15:35:02 -0500

    final

---
+++ README.md
@@ -0,0 +1,32 @@
+# Space invaders
+Coding DOJO bootcamp project, 2d platformer game
+
+## Getting Started
+Try to shoot down all enemies ship, before they approach you.
+
+## Controls
+* move ⬆️/W 
+* move ➡️/D
+* move ⬇️/S 
+* move ⬅️/A 
+* shot SPACE/R
+
+## Features added
+* changed default background;
+* changed spaceships graphics;
+* added borders;
+* added missile collision;
+* added won notification;
+
+## Author
+
+* **IURII LYTVYN** 
+
+## License
+
+This project is licensed under the MIT License 
+
+## Demo
+![](public/img/demo.png)
+
+
--- public/js/app.js
+++ public/js/app.js
@@ -4,61 +4,64 @@ let player = {
 };
 
 let enemies = [
-  { left: 250, top: 20 },
-  { left: 350, top: 80 },
+  { left: 300, top: 20 },
+  { left: 375, top: 80 },
   { left: 450, top: 130 },
-  { left: 550, top: 80 },
-  { left: 650, top: 20 },
+  { left: 525, top: 80 },
+  { left: 600, top: 20 },
 ];
+
+let missiles = [];
 
 function drawPlayer() {
   content =
-    "<div class='player' style='left:" +
-    player.left +
-    "px; top: " +
-    player.top +
-    "px'></div>";
+    "<div class='player' style='left:" + player.left + "px; top: " + player.top + "px'></div>";
   document.getElementById("players").innerHTML = content;
 }
 
 function drawEmenies() {
   content = "";
   for (let i = 0; i < enemies.length; i++) {
-    content +=
-      "<div class='enemy' style='left:" +
-      enemies[i].left +
-      "px; top: " +
-      enemies[i].top +
-      "px'></div>";
+    content += "<div class='enemy' style='left:" + enemies[i].left + "px; top: " + enemies[i].top + "px'></div>";
   }
   document.getElementById("enemies").innerHTML = content;
 }
 
+function drawMissiles() {
+  content = "";
+  for (let i = 0; i < missiles.length; i++) {
+    content += "<div class='missile' style='left:" + missiles[i].left + "px; top: " + missiles[i].top + "px'></div>"; 
+  }
+  document.getElementById("missiles").innerHTML = content;
+  
+}
+
 function moveEnemies() {
   let speed = Math.floor(Math.random()*10);
-  console.log(`speed:${speed}px`);
+  console.log(`enemy approaching:${speed}px`);
   for (let i = 0; i < enemies.length; i++) {
     enemies[i].top = enemies[i].top + speed;
   }
   document.getElementById("enemies").innerHTML = content;
 }
 
-
-function drawMissiles() {
-  console.log('shots fired');
+function moveMissiles() {
+//  let speed = Math.floor(Math.random() * 20);
+  for (let i = 0; i < missiles.length; i++) {
+    missiles[i].top = missiles[i].top - 20;
+  }
+  document.getElementById("missiles").innerHTML = content;
 }
 
 function borderControl() {
-  console.log(`top: ${player.top}`);
-  console.log(`left: ${player.left}`);
-
+  console.log(`top: ${player.top}/left: ${player.left}`);
   if (player.top === 300) {
     player.top = 400;
     drawPlayer();
   } else if (player.top === 620) {
     player.top = 600;
     drawPlayer();
-  } else if (player.left === 820) {
+  } else if (player.left === 900) {
     player.left = 800;
     drawPlayer();
   } else if (player.left === 10) {
@@ -73,7 +76,7 @@ function keyControl() {
     const rightMoveKey = event.keyCode === 39 || event.keyCode === 68;
     const downMoveKey = event.keyCode === 40 || event.keyCode === 83;
     const upMoveKey = event.keyCode === 38 || event.keyCode === 87;
-    const fireKey = event.keyCode === 82;
+    const fireKey = event.keyCode === 82 || event.keyCode === 32;
 
     if (leftMoveKey) {
       player.left -= 10;
@@ -88,22 +91,43 @@ function keyControl() {
       player.top -= 10;
     }
     if (fireKey) {
+      console.log(`shots fired 🔫`);
+      missiles.push({ left: player.left + 34, top: player.top + 8 });
       drawMissiles()
     }
-
     borderControl();
     drawPlayer();
   };
 }
 
-
+function targetHit() {
+  for (let enemy = 0; enemy < enemies.length; enemy++) {
+      for (let missile = 0; missile < missiles.length; missile++) {
+          if ( 
+              missiles[missile].left >= enemies[enemy].left  &&
+              missiles[missile].left <= (enemies[enemy].left + 50)  &&
+              missiles[missile].top <= (enemies[enemy].top + 50)  &&
+              missiles[missile].top >= enemies[enemy].top
+          ) {
+              enemies.splice(enemy, 1);
+              missiles.splice(missile, 1);
+              if (enemies.length === 0) {
+                alert('YOU WON !!!');
+              }
+          }
+      }
+  }
+}
 
 function gameLoop() {
   keyControl();
   drawPlayer();
   moveEnemies();
   drawEmenies();
-  
-  setTimeout(gameLoop, 500);
+  moveMissiles()
+  drawMissiles()
+  targetHit();
+  setTimeout(gameLoop, 100);
 }
+
 gameLoop();
